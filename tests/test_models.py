f8d636c035ee6c08b7578d00dfc9bd377a49a5f6
Chris Talkington <chris@talkingtontech.com>
2020-05-01 18:19:45 -0500

    Create test_models.py

---
+++ tests/test_models.py
@@ -0,0 +1,49 @@
+"""Tests for Roku Models."""
+from typing import List
+
+import pytest
+import rokuecp.models as models
+import xmltodict
+from rokuecp import RokuError
+
+from . import load_fixture
+
+INFO = xmltodict.parse(load_fixture("device-info.xml"))
+APPS = xmltodict.parse(load_fixture("apps.xml"))
+
+DEVICE = {"info": INFO, "apps": APPS["apps"]["app"]}
+
+
+def test_device() -> None:
+    """Test the Device model."""
+    device = models.Device(APPLICATION)
+
+    assert device
+
+    assert device.info
+    assert isinstance(device.info, models.Info)
+
+
+def test_device_no_data() -> None:
+    """Test the Device model."""
+    with pytest.raises(RokuError):
+        models.Device({})
+
+
+def test_info() -> None:
+    """Test the Info model."""
+    info = models.Info.from_dict(INFO)
+
+    assert info
+    assert info.name == ""
+    assert info.version == ""
+
+
+def test_application() -> None:
+    """Test the Application model."""
+    app = models.Application.from_dict(APPS[0])
+
+    assert app
+    assert app.app_id == ""
+    assert app.name == ""
+
