cb01f7beff4f399a0c62d9cea5695ee564915a0b
Chris Talkington <chris@talkingtontech.com>
2020-05-01 17:44:35 -0500

    Update models.py

--- rokuecp/models.py
+++ rokuecp/models.py
@@ -67,7 +67,7 @@ class Device:
             self.info = Info.from_dict(data["info"])
 
         if "apps" in data and data["apps"]:
-            apps = [Application.from_dict(app_data) for app_data in data["apps"]]
+            apps = [Application.from_dict(app_data) for app_data in data["apps"]["app"]]
             self.apps = apps
 
         return self
