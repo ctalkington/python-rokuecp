0fb7066b896d8558d13b1a804330d6d2ed025e11
Chris Talkington <chris@talkingtontech.com>
2020-05-01 18:39:16 -0500

    Update rokuecp.py

--- rokuecp/rokuecp.py
+++ rokuecp/rokuecp.py
@@ -122,7 +122,7 @@ class Roku:
             if apps is None or "apps" not in apps:
                 raise RokuError("Roku device returned an empty API response")
 
-            self._device = Device({"info": info, "apps": apps["apps"]["app"]})
+            self._device = Device({"info": info["device-info"], "apps": apps["apps"]["app"]})
             return self._device
 
         self._device.update_from_dict({})
