a5d13641c04a0b4766a2aadb8f6fbaa70d751ae5
Chris Talkington <chris@talkingtontech.com>
2020-04-30 23:00:46 -0500

    Create test_interface.py

---
+++ tests/test_interface.py
@@ -0,0 +1,87 @@
+"""Tests for Roku."""
+from typing import List
+
+import pytest
+import roku.models as models
+from aiohttp import ClientSession
+from roku import Roku
+
+from . import load_fixture
+
+HOST = "192.168.1.86"
+PORT = 8060
+
+MATCH_HOST = f"{HOST}:{PORT}"
+
+
+@pytest.mark.asyncio
+async def test_device(aresponses):
+    """Test app property is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/query/device-info",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/xml"},
+            text=load_fixture("device-info.xml"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/query/apps",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/xml"},
+            text=load_fixture("apps.xml"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        client = Roku(HOST, session=session)
+        await client.update()
+
+        assert client.app
+        assert isinstance(client.device, models.Device)
+
+
+@pytest.mark.asyncio
+async def test_update(aresponses):
+    """Test update method is handled correctly."""
+    aresponses.add(
+        MATCH_HOST,
+        "/query/device-info",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/xml"},
+            text=load_fixture("device-info.xml"),
+        ),
+    )
+
+    aresponses.add(
+        MATCH_HOST,
+        "/query/apps",
+        "GET",
+        aresponses.Response(
+            status=200,
+            headers={"Content-Type": "application/xml"},
+            text=load_fixture("apps.xml"),
+        ),
+    )
+
+    async with ClientSession() as session:
+        client = Roku(HOST, session=session)
+        response = await client.update()
+
+        assert response
+        assert isinstance(response.info, models.Info)
+        assert isinstance(response.apps, List)
+
+        response = await client.update()
+
+        assert response
+        assert isinstance(response.info, models.Info)
+        assert isinstance(response.apps, List)
