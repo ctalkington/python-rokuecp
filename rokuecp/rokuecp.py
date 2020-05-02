2f625c77e0c4533bb2f5f6ba052d0a4ec5f5e342
Chris Talkington <chris@talkingtontech.com>
2020-05-02 14:40:38 -0500

    Update rokuecp.py

--- rokuecp/rokuecp.py
+++ rokuecp/rokuecp.py
@@ -113,7 +113,7 @@ class Roku:
 
     async def update(self) -> Device:
         """Get all information about the device in a single call."""
-        dict: updates = {
+        updates: dict = {
             "app": None,
             "apps": [],
             "channel": None,
