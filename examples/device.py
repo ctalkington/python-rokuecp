df749282491bfecdba5445e9c2c6b4b4240d58e8
Chris Talkington <chris@talkingtontech.com>
2020-05-09 09:49:21 -0500

    Create device.py

---
+++ examples/device.py
@@ -0,0 +1,20 @@
+# pylint: disable=W0621
+"""Asynchronous Python client for IPP."""
+import asyncio
+
+from rokuecp import Roku
+
+
+async def main():
+    """Show example of connecting to your IPP print server."""
+    async with Roku("192.168.1.81") as roku:
+        # Get Roku Device Info
+        device = await roku.update()
+        print(device.info)
+
+        await roku.remote("Home")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
