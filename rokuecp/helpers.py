8d9dc9899d43f2bacbab1044745ca3250b0fa101
Chris Talkington <chris@talkingtontech.com>
2021-07-11 12:54:57 -0500

    Create helpers.py

---
+++ rokuecp/helpers.py
@@ -0,0 +1,24 @@
+"""Helpers for Roku Client."""
+from ip_address import ip_address
+from socket import gaierror as SocketGIAError, gethostbyname
+
+from .exceptions import RokuConnectionError
+
+
+def is_ip_address(host: str) -> bool:
+    """Determine if host is an IP Address."""
+    try:
+        ip_address(host)
+    except ValueError:
+        return False
+
+   return True
+
+def resolve_hostname(host: str) -> str:
+    """Resolve hostname to IP Address."""
+    try:
+        return gethostbyname(host)
+    except SocketGIAError as exception:
+        RokuConnectionError(
+            f"Error occurred while resolving hostname: {host}"
+       ) from exception
