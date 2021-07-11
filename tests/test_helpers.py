c8811ef010aacce66314870a531aabdf2dab47d9
Chris Talkington <chris@talkingtontech.com>
2021-07-11 14:49:56 -0500

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,27 +5,26 @@ import pytest
 from rokuecp import RokuConnectionError
 from rokuecp.helpers import is_ip_address, resolve_hostname
 
-MOCK_HOSTNAME = "roku.local"
-MOCK_HOST = "192.168.1.2"
+HOSTNAME = "roku.local"
+HOST = "192.168.1.2"
 
 
 def test_is_ip_address() -> None:
     """Test the is_ip_address helper."""
-    assert is_ip_address(MOCK_HOST)
-    assert not is_ip_address("roku.local")
+    assert is_ip_address(HOST)
+    assert not is_ip_address(HOSTNAME)
 
 
 def test_resolve_hostname() -> None:
     """Test the resolve_hostname helper."""
     with pytest.mock.patch(
-        "rokuecp.helpers.gethostbyname", return_value=MOCK_HOST
+        "rokuecp.helpers.gethostbyname", return_value=HOST
     ) as mock_gethostbyname:
-        assert resolve_hostname(MOCK_HOSTNAME) == MOCK_HOST
+        assert resolve_hostname(HOSTNAME) == HOST
         assert len(mock_gethostbyname.mock_calls) == 1
           
     with pytest.raises(RokuConnectionError):
         with pytest.mock.patch(
             "rokuecp.helpers.gethostbyname", side_effect=SocketGIAError()
         ):
-            resolve_hostname(MOCK_HOSTNAME)
-
+            resolve_hostname(HOSTNAME)
