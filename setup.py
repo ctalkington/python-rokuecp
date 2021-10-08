5a964e01ffd13a610a745b008586b05dc2cc25b4
Chris Talkington <chris@talkingtontech.com>
2021-10-07 22:05:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
