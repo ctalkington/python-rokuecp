7280b72e08bf481ec2be5bbe48a0d872e0da5e9e
Chris Talkington <chris@talkingtontech.com>
2021-11-25 15:39:33 -0600

    Detect if device supports finding remote (#251)
    * detect if device supports finding remote
    
    * Update test_models.py
    
    * Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -122,6 +122,7 @@ def test_info() -> None:
     assert info.ethernet_support
     assert info.ethernet_mac == "b0:a7:37:96:4d:fa"
     assert info.wifi_mac == "b0:a7:37:96:4d:fb"
+    assert not info.supports_find_remote
     assert not info.supports_private_listening
     assert not info.headphones_connected
     assert info.version == "7.5.0"
@@ -144,6 +145,7 @@ def test_info_stick_3500x() -> None:
     assert not info.ethernet_support
     assert info.ethernet_mac is None
     assert info.wifi_mac == "b0:a7:37:6a:ec:2d"
+    assert info.supports_find_remote
     assert not info.supports_private_listening
     assert not info.headphones_connected
     assert info.version == "10.0.0"
@@ -166,6 +168,7 @@ def test_info_tv_7820x() -> None:
     assert info.ethernet_support
     assert info.ethernet_mac == "d4:3a:2e:07:fd:cb"
     assert info.wifi_mac == "d8:13:99:f8:b0:c6"
+    assert info.supports_find_remote
     assert info.supports_private_listening
     assert not info.headphones_connected
     assert info.version == "9.2.0"
@@ -188,6 +191,7 @@ def test_info_tv_d803x() -> None:
     assert not info.ethernet_support
     assert info.ethernet_mac is None
     assert info.wifi_mac == "d4:ab:cd:2f:6b:55"
+    assert not info.supports_find_remote
     assert info.supports_private_listening
     assert not info.headphones_connected
     assert info.version == "10.0.0"
