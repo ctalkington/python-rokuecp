be969f45c0b0e7b0eab5e1fc8dd4ee9bd52a9936
Chris Talkington <chris@talkingtontech.com>
2021-12-21 00:53:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     description="Asynchronous Python client for Roku (ECP).",
     include_package_data=True,
     install_requires=list(
-        val.strip() for val in open("requirements.txt", encoding="utf-8")  # pylint: disable=consider-using-wit
+        val.strip() for val in open("requirements.txt", encoding="utf-8")  # pylint: disable=consider-using-with
     ),
     keywords=["roku", "api", "async", "client", "ecp"],
     license="MIT license",
