518813c1ddc8d44a5d506e9a253ff7095219c2ae
Chris Talkington <chris@talkingtontech.com>
2021-12-24 16:21:06 -0600

    Restore coverage to 100% (#265)

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,11 +1,12 @@
 """Tests for Roku Client Helpers."""
 from ipaddress import ip_address
+from socket import gaierror as SocketGIAError
 
 import pytest
 from rokuecp.exceptions import RokuConnectionError
 from rokuecp.helpers import is_ip_address, resolve_hostname
 
-from . import patch_resolver_loop
+from . import fake_addrinfo_results
 
 HOSTNAME = "roku.local"
 HOST = "192.168.1.2"
@@ -18,23 +19,28 @@ def test_is_ip_address() -> None:
 
 
 @pytest.mark.asyncio
-async def test_resolve_hostname() -> None:
+async def test_resolve_hostname(resolver) -> None:
     """Test the resolve_hostname helper."""
-    with patch_resolver_loop([HOST]):
-        result = await resolve_hostname(HOSTNAME)
-        assert result == HOST
-        ip_address(result)
+    resolver.return_value = fake_addrinfo_results([HOST])
+
+    result = await resolve_hostname(HOSTNAME)
+    assert result == HOST
+    ip_address(result)
 
 
 @pytest.mark.asyncio
-async def test_resolve_hostname_error_invalid() -> None:
+async def test_resolve_hostname_error_invalid(resolver) -> None:
     """Test the resolve_hostname helper fails properly."""
-    with pytest.raises(RokuConnectionError), patch_resolver_loop(["not-an-ip"]):
+    resolver.return_value = fake_addrinfo_results(["not-an-ip"])
+
+    with pytest.raises(RokuConnectionError):
         await resolve_hostname("error.local")
 
 
 @pytest.mark.asyncio
-async def test_resolve_hostname_error_not_found() -> None:
+async def test_resolve_hostname_error_not_found(resolver) -> None:
     """Test the resolve_hostname helper fails properly."""
-    with pytest.raises(RokuConnectionError), patch_resolver_loop():
+    resolver.side_effect = SocketGIAError
+
+    with pytest.raises(RokuConnectionError):
         await resolve_hostname("error.local")
