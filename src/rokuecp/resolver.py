31c8f57e313652be5585548f783b1554dc569f1c
Chris Talkington <chris@talkingtontech.com>
2022-02-04 15:34:08 -0600

    Update resolver.py

--- src/rokuecp/resolver.py
+++ src/rokuecp/resolver.py
@@ -1,4 +1,5 @@
 """DNS Resolver for Roku Client based on aiohttp logic."""
+# type: ignore
 from __future__ import annotations
 
 import socket
@@ -47,7 +48,7 @@ class ThreadedResolver:
 
         hosts = []
         for _family, _, proto, _, address in infos:
-            if _family == socket.AF_INET6 and len(address) > 2 and address[3]:  # pragma: no cover
+            if _family == socket.AF_INET6 and address[3]:  # pragma: no cover
                 # LL IPv6 is a VERY rare case.
                 raise OSError("link-local IPv6 addresses not supported")
 
