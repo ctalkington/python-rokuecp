1bfde2b2f637a635c081946877f62bb267705ec0
Chris Talkington <chris@talkingtontech.com>
2022-02-13 17:05:49 -0600

    black

--- src/rokuecp/helpers.py
+++ src/rokuecp/helpers.py
@@ -23,7 +23,7 @@ MIME_TO_STREAM_FORMAT = {
 
 def guess_stream_format(url: str, mime_type: str | None = None) -> str | None:
     """Guess the Roku stream format for a given URL and MIME type.
-    
+
     Params:
         url: The URL to determine stream format for.
         mime_type: The MIME type to aid in stream format determination.
