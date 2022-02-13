3912f8aa50ef02c4ba896633de748eddbaaf37d1
Chris Talkington <chris@talkingtontech.com>
2022-02-13 17:22:16 -0600

    Add helper to guess stream format (#308)

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,12 +4,34 @@ from socket import gaierror as SocketGIAError
 
 import pytest
 from rokuecp.exceptions import RokuConnectionError
-from rokuecp.helpers import is_ip_address, resolve_hostname
+from rokuecp.helpers import guess_stream_format, is_ip_address, resolve_hostname
 
 from . import fake_addrinfo_results
 
 HOSTNAME = "roku.local"
 HOST = "192.168.1.2"
+
+
+def test_guess_stream_format() -> None:
+    """Test the guess_stream_format helper."""
+    assert guess_stream_format("/path/media.media") is None
+    assert guess_stream_format("/path/media.txt") is None
+
+    assert guess_stream_format("/path/media.mp4", "video/mp4") == "mp4"
+    assert guess_stream_format("/path/media.mp4") == "mp4"
+    assert guess_stream_format("/path/media.m4v") == "mp4"
+    assert guess_stream_format("/path/media.mov") == "mp4"
+    assert guess_stream_format("/path/media.mkv") == "mkv"
+    assert guess_stream_format("/path/media.mks") == "mks"
+    assert guess_stream_format("/path/media.m3u8") == "hls"
+    assert guess_stream_format("/path/media.dash") == "dash"
+    assert guess_stream_format("/path/media.mpd") == "dash"
+    assert guess_stream_format("/path/media.ism/manifest") == "ism"
+
+    assert guess_stream_format("/path/media.mp3") == "mp3"
+    assert guess_stream_format("/path/media.m4a") == "m4a"
+    assert guess_stream_format("/path/media.mka") == "mka"
+    assert guess_stream_format("/path/media.wma") == "wma"
 
 
 def test_is_ip_address() -> None:
