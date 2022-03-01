b7cfdfbbe8acadc95233ac89f5647bace87fe718
Chris Talkington <chris@talkingtontech.com>
2022-02-28 21:51:50 -0600

    Update __init__.py

--- src/rokuecp/__init__.py
+++ src/rokuecp/__init__.py
@@ -1,4 +1,4 @@
 """Asynchronous Python client for Roku."""
-from .exceptions import RokuConnectionError, RokuError  # noqa
+from .exceptions import RokuConnectionError, RokuConnectionTimeoutError, RokuError  # noqa
 from .models import Application, Channel, Device, Info, MediaState, State  # noqa
 from .rokuecp import Roku  # noqa
