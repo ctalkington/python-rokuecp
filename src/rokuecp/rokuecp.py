36536a3f56ce526ac7096c235e3a77487ad60773
Chris Talkington <chris@talkingtontech.com>
2024-01-29 21:54:40 -0600

    Update rokuecp.py

--- src/rokuecp/rokuecp.py
+++ src/rokuecp/rokuecp.py
@@ -345,8 +345,8 @@ class Roku:
         ------
             RokuError: Received an unexpected response from the Roku device.
         """
-        if key[:3] == "Lit":
-            return self.literal(key[3:)
+        if key[:4] == "Lit_":
+            return self.literal(key[4:)
 
         key_lower = key.lower()
         if key_lower not in VALID_REMOTE_KEYS:
